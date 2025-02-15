38cd0a285ecef6f9656ee1c39eac804c3c6b8012
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-02-14 21:04:41 -0500

    Time: 1989 ms (5.06%) | Memory: 17.7 MB (92.56%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -1,35 +1,30 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
         res = 0
-        cache = {}
         def solve(x):
-            nonlocal cache
             target = x*x
             num = str(target)
             arr = [ch for ch in num]
             n = len(arr)
             # print('X, target',x,target,arr)
             
-            def recur(curr,prev,total,val):
+            def recur(curr,prev,total):
                 # print(curr,prev,total)
                 if curr==n and total==x and curr==prev:
                     return True
                 if curr>=n:
                     return False
-                if (curr,prev,total,val) in cache:
-                    return cache[(curr,prev,total,val)]
                 # take curr
                 temp = ''
                 for i in range(prev, curr+1):
                     temp+=arr[i]
                 res = False
-                res |= recur(curr+1,curr+1,total+int(temp),val)
+                res |= recur(curr+1,curr+1,total+int(temp))
                 # leave curr
-                res |= recur(curr+1,prev,total,val)
-                cache[(curr,prev,total,val)] = res
-                return cache[(curr,prev,total,val)]
+                res |= recur(curr+1,prev,total)
+                return res
 
-            res = recur(0,0,0,x)
+            res = recur(0,0,0)
             # print('Res',res)      
             return target if res else 0                 
 
