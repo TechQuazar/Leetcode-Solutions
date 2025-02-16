fadb8fc84ab51c6caf115d60b39e6a8b2fff7ffe
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-02-16 13:56:41 -0500

    Time: 1810 ms (5.00%) | Memory: 332.7 MB (5.02%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -16,4 +16,21 @@ class Solution:
             for j in range(i,n):
                 res+= recur(i,j)
 
+        res = 0
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = 1
+            res+=1
+        
+        for i in range(n-1,-1,-1):
+            for j in range(i,n):
+                if i==j:
+                    continue
+                if i+1==j:
+                    dp[i][j] = 1 if s[i]==s[j] else 0
+                else:
+                    dp[i][j] = dp[i+1][j-1] if s[i]==s[j] else 0
+                res+=dp[i][j]
+
         return res
