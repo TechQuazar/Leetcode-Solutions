ca69b2c52282b23f38af327f00b0995e90baa549
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-02-16 16:17:13 -0500

    Time: 4 ms (10.48%) | Memory: 17.7 MB (70.00%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,23 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        def isValid(st):
-            if st[0]!='0' and int(st) in range(1,27):
-                return True
-            return False
         cache = {}
-        def recur(i,prevIdx):
+        def recur(i):
             if i==n:
-                if i==prevIdx:
-                    return 1
-                else:
-                    return 0
-            if (i,prevIdx) in cache:
-                return cache[(i,prevIdx)]
-            
-            take = recur(i+1,i+1) if isValid(s[prevIdx:i+1]) else 0
-            total = take + recur(i+1,prevIdx)
-            cache[(i,prevIdx)]= total
-            return cache[(i,prevIdx)]
-        
-        return recur(0,0)
+                return 1
+            total = 0
+            if i in cache:
+                return cache[i]
+            if s[i]!='0':
+                total+=recur(i+1)
+            if i+1<n and (s[i]=='1' or s[i]=='2' and s[i+1]<='6'):
+                total+= recur(i+2)
+            cache[i] = total
+            return total
+
+        return recur(0)
