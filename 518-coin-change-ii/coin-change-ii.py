05422b597830040c2656fe31b4435555cd45dd85
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-02-18 17:09:57 -0500

    Time: 436 ms (34.00%) | Memory: 61.2 MB (32.41%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -13,4 +13,16 @@ class Solution:
             # take coin, leave coin
             cache[(i,total)] = recur(i,total+coins[i]) + recur(i+1,total)
             return cache[(i,total)]
-        return recur(0,0)
+
+        dp = [[0 for _ in range(amount+1)] for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0] = 1 # if amount==1, then only 1 way to make it 1 by choosing nothing
+        
+        for i in range(1,n+1):
+            for j in range(1,amount+1):
+                dp[i][j] = dp[i-1][j]
+                if j>=coins[i-1]:
+                    dp[i][j] += dp[i][j-coins[i-1]]
+
+        
+        return dp[n][amount]
