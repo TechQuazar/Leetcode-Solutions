182f346b59ba6c36e60fa829cd52507ba0ff4efe
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-04-05 20:24:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.84%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -4,13 +4,25 @@ class Solution:
         repeated work
         5 1 6
         brute force o(2**n)
+        --
+        summing all subsets... XOR different bits = 1... we always will have 1 bit set from the any number being considered as a single subset...
+        each num is gonna appear in half of the subsets. Therefore, we can count the bits that are set by that subset, and multiply them by (total # of subsets)/2 == 2**n /2 == 2** (n-1)
         '''
-        total = 0
+        res = 0
         n = len(nums)
-        def recur(i, curr):
-            if i>=n:
-                return curr
-            return recur(i+1,curr^nums[i]) + recur(i+1,curr)
-        return recur(0,0)
+        half = 2**(n-1)
+        seen = set()
+        for num in nums:
+            bitIdx = 0
+            temp = num
+            while temp!=0:
+                if bitIdx not in seen and temp & 0x1 == 1:
+                    res+= half*(2**bitIdx)
+                    seen.add(bitIdx)
+                bitIdx+=1
+                temp = temp>>1
+        return res
+            
+        
 
         
