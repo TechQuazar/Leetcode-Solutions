999076b6cdf3d709d30e70ccf7ddb5395d6fc458
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-04-05 20:26:15 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.94%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -7,21 +7,15 @@ class Solution:
         --
         summing all subsets... XOR different bits = 1... we always will have 1 bit set from the any number being considered as a single subset...
         each num is gonna appear in half of the subsets. Therefore, we can count the bits that are set by that subset, and multiply them by (total # of subsets)/2 == 2**n /2 == 2** (n-1)
+        --
+        OPT O(n)
+        use OR to gather bits
         '''
-        res = 0
-        n = len(nums)
-        half = 2**(n-1)
-        seen = set()
+        total = 0
+        half = 2**(len(nums)-1)
         for num in nums:
-            bitIdx = 0
-            temp = num
-            while temp!=0:
-                if bitIdx not in seen and temp & 0x1 == 1:
-                    res+= half*(2**bitIdx)
-                    seen.add(bitIdx)
-                bitIdx+=1
-                temp = temp>>1
-        return res
+            total |= num
+        return total * half
             
         
 
