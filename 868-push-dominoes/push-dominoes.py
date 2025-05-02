aca8a0c5402c1836f67bd66c96f23675516998b7
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-05-02 08:41:49 -0400

    Time: 123 ms (80.59%) | Memory: 20 MB (79.93%) - LeetSync

--- 868-push-dominoes/push-dominoes.py
+++ 868-push-dominoes/push-dominoes.py
@@ -1,54 +1,26 @@
 class Solution:
     def pushDominoes(self, s: str) -> str:
-        """
+        '''
         .L.R...LR..L..
-        LL.RR.LLRRLL..
-        BFE - O(n*k) iterate through all keep updating state one by one till you reach same
-        OPT - O(k*k) only iterate through the unchanged ones each step of state
-        """
-        # BFE
-        seen  = set()
+
+        '''
+        s = 'L'+s+'R'
         n = len(s)
-        stable = set()
-        print("S",s)
-        while True:
-            temp = ""
-            remain = False
-            for i in range(n):
-                curr = s[i]
-                if curr=='.' and i not in stable:
-                    if i==0:
-                        # only force from the right one
-                        if i+1 <n and s[i+1]=='L':
-                            curr="L"
-                        elif i+1<n and s[i+1]=='.':
-                            remain = True
+        res = ""
+        i = 0
+        for j in range(1,n):
+            mid = j-i-1
+            if s[j]=='.':
+                continue
+            if i>0: # append the left domino
+                res+=s[i]
+            # process middle part
+            if s[i]==s[j]:
+                res+= s[i]*mid
+            elif s[i]=='L' and s[j]=='R':
+                res+= '.'*mid
+            elif s[i]=='R' and s[j]=='L':
+                res+= 'R'*(mid//2) + '.'*(mid%2) + 'L'*(mid//2)
+            i=j           
 
-                    elif i==n-1:
-                        # only force from the left one
-                        if i-1>=0 and s[i-1]=='R':
-                            curr="R"
-                        elif i-1>=0 and s[i-1]=='.':
-                            remain = True
-
-                    else:
-                        # possible force from both
-                        if i-1>=0 and i+1<n and s[i-1]=='R' and s[i+1]=='L':
-                            curr="."
-                        elif i-1>=0 and s[i-1]=='R':
-                            curr = 'R'
-                        elif i+1<n and s[i+1]=='L':
-                            curr = 'L'
-                        else:
-                            remain = True
-                if not remain:
-                    stable.add(i)
-                temp+=curr
-            s = temp
-            print('S',s)
-            if s in seen:
-                return s
-            else:
-                seen.add(s)
-    
-        
+        return res
