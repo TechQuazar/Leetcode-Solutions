5138939350ee6f02d13cda515f14d592008f5505
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-05-12 21:23:47 -0400

    Time: 14 ms (41.44%) | Memory: 20.5 MB (18.60%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -6,31 +6,21 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         '''
-        for each LL, need to track which node we are at. Can maintain HashMap for that
-        O(total nodes => k*(avg nodes))
+        PQ
         '''
-        LLidx = defaultdict(ListNode)
+        pq = []
+        for i,node in enumerate(lists):
+            if node != None:
+                heapq.heappush(pq,(node.val,i,node)) # i because when values are same, we can use idx to compare
+        
         res = ListNode()
         prev = res
-        n = len(lists)
-        for i in range(n):
-            LLidx[i] = lists[i] if lists[i] != None else None
-        #print('LLidx',LLidx)
-        completed = set()
-        while len(completed)<n:
-            minVal = float('inf')
-            minListIdx = -1
-            for i in range(n):
-                if LLidx[i]!= None and LLidx[i].val < minVal:
-                    minVal = LLidx[i].val
-                    minListIdx = i
-            if minListIdx==-1:
-                return res.next
-            currNode = ListNode(minVal)
-            LLidx[minListIdx] = LLidx[minListIdx].next
-            if LLidx[minListIdx]==None:
-                    completed.add(minListIdx)
+        while pq:
+            val,i, node = heapq.heappop(pq)
+            currNode = ListNode(val)
             prev.next = currNode
             prev = currNode
+            if node.next!=None:
+                heapq.heappush(pq,(node.next.val,i,node.next))
 
         return res.next
