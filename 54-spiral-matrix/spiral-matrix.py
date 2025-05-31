3a698bb191d8b76f1a8080b35030cd05f574c9a3
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-05-31 09:50:55 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (62.34%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,22 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        m,n = len(matrix),len(matrix[0])
-        l,r = 0, n
-        top,bottom = 0,m
-        res =[]
-        while l<r and top<bottom:
-            for i in range(l,r):
-                res.append(matrix[top][i])
-            top+=1
-            for i in range(top,bottom):
-                res.append(matrix[i][r-1])
-            r-=1
-            if not (l<r and top<bottom):
-                return res
-            for i in range(r-1,l-1,-1):
-                res.append(matrix[bottom-1][i])
-            bottom-=1
-            for i in range(bottom-1,top-1,-1):
-                res.append(matrix[i][l])
-            l+=1
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, n - 1
+        t, b = 0, m - 1
+        res = []
+
+        while l <= r and t <= b:
+            for i in range(l, r + 1):
+                res.append(matrix[t][i])
+            for i in range(t + 1, b + 1):
+                res.append(matrix[i][r])
+            if t < b:
+                for i in range(r - 1, l - 1, -1):
+                    res.append(matrix[b][i])
+            if l < r:
+                for i in range(b - 1, t, -1):
+                    res.append(matrix[i][l])
+            l += 1
+            r -= 1
+            t += 1
+            b -= 1
+
         return res
