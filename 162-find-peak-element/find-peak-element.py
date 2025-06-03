82a2f7b40d1a85482bbbb622fb1abd6cd309da83
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-02 20:42:38 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (38.30%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        l,r = 0,n-1
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid]>nums[mid+1]:
+                # peak must exist on the left as well
+                r = mid
+            elif nums[mid]<nums[mid+1]:
+                l = mid+1
+        return l
+        
