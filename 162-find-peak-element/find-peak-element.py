83c7a5770b5abbf3b6aab5d1fecd2bda0583af90
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-02 20:43:30 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (38.30%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,13 +1,22 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        l,r = 0,n-1
-        while l<r:
-            mid = (l+r)//2
-            if nums[mid]>nums[mid+1]:
-                # peak must exist on the left as well
-                r = mid
-            elif nums[mid]<nums[mid+1]:
-                l = mid+1
-        return l
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            left_neighbor = nums[mid - 1] if mid > 0 else float('-inf')
+            right_neighbor = nums[mid + 1] if mid < len(nums) - 1 else float('-inf')
+            
+            # Check if mid is a peak
+            if nums[mid] > left_neighbor and nums[mid] > right_neighbor:
+                return mid
+            
+            # If right neighbor is greater, move right
+            if nums[mid] < right_neighbor:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1  # Just a fallback; shouldn't happen due to the problem guarantee
         
