df8773bb03645f3d79853d8f6fad275f258c26fd
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-02 23:19:16 -0400

    Time: 112 ms (84.60%) | Memory: 78.6 MB (27.44%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,92 +1,43 @@
 class Node:
-    def __init__(self,key, val):
-        self.key = key
-        self.val = val
-        self.next = None
-        self.prev = None
+    def __init__(self,key,val):
+        self.key,self.val = key,val
+        self.prev=self.next=None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        '''
-        LL - double
-        start -> LR node
-        curr -> MR node
-        lookup -> dict mapping from key -> LL node
-        1. Get
-        - check lookup, if present, return val for key. Delete that node, connect the rest, add new node
-        in the front. Need doubly LL for O(1) deletion
-        - if not present, -1
-        2. PUT
-        - check lookup, if present, update val. Delete that node, connect the rest, add new node
-        in the front
-        - if not, add node to front and update lookup. If cap exceeded, delete last node.
-        '''
-        self.capacity = capacity
-        self.last = Node(-1,-1) # return next of last for LRnode
-        self.curr = None
-        self.lookup = defaultdict(Node)
-        self.size = 0
+        self.n = capacity
+        self.cache = {}
+        self.left,self.right = Node(0,0), Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self,node):
+        prev,nxt = node.prev,node.next
+        prev.next, nxt.prev = nxt,prev
     
-    def deleteAndConnect(self,node):
-        # delete this node, then add it to front
-        # if node is curr, keep as is:
-        if self.curr == node:
-            return
-        prevNode = node.prev
-        nextNode = node.next
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-        newNode = Node(node.key, node.val)
-        self.curr.next = newNode
-        newNode.prev = self.curr
-        self.curr = newNode
-        self.lookup[node.key] = newNode
-        del node
+    def insert(self,node):
+        prev,nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt,prev
 
     def get(self, key: int) -> int:
-        
-        if key in self.lookup:
-            res = self.lookup[key].val
-            self.deleteAndConnect(self.lookup[key])
-            # print('GET:',self.lookup)
-            return res
-        # print('GET:',self.lookup)
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        
-        if key in self.lookup:
-            # no need to check size
-            node = self.lookup[key]
-            node.val = value
-            self.deleteAndConnect(self.lookup[key])
-            # print('PUT:',self.lookup)
-            return
-        # add node
-        newNode = Node(key,value)
-        self.lookup[key]= newNode
-        if self.curr is None:
-            self.curr = newNode
-            self.last.next = self.curr
-            self.curr.prev = self.last
-        else:
-            self.curr.next = newNode
-            newNode.prev = self.curr
-            self.curr = newNode
-        self.size+=1
-        if self.size>self.capacity:
-            lastNode = self.last.next
-            lastNode.next.prev = self.last
-            self.last.next = lastNode.next
-            del self.lookup[lastNode.key]
-            del lastNode
-            self.size-=1
-        # print('PUT:',self.lookup)
-        return
-        
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+        if len(self.cache) > self.n:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
 
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
