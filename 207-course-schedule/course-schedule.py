b83a365f38c0225b533ee80cd815c5e80e08b6df
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-08 00:52:26 -0400

    Time: 3 ms (88.17%) | Memory: 19.1 MB (61.20%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,30 +1,18 @@
 class Solution:
-    def canFinish(self, n: int, arr: List[List[int]]) -> bool:
-        adj = {}
-        for i in range(n):
-            adj[i] = []
-        for u,v in arr:
-            adj[u].append(v)
+    def canFinish(self, n: int, prereq: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        indegree = [0]*n
+        for u,v in prereq:
+            adj[v].append(u)
+            indegree[u]+=1
         
-        cache = {}
-        def dfs(node,parent):
-            if adj[node] == []:
-                return True
-            if node in cache:
-                return cache[node]
-            visited.add((parent,node))
-            counter = 0
-            for prereq in adj[node]:
-                if (node,prereq) in visited:
-                    return False
-                if dfs(prereq,node):
-                    counter+=1
-            canTake = True if counter==len(adj[node]) else False
-            cache[node] = canTake
-            return canTake
-        # print('ADJ',adj)
-        res = True
-        for i in range(n):
-            visited = set()
-            res &= dfs(i,-1)
-        return res
+        q = deque([i for i in range(n) if indegree[i]==0])
+        count = 0
+        while q:
+            curr = q.popleft()
+            count+=1
+            for nei in adj[curr]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        return count==n
