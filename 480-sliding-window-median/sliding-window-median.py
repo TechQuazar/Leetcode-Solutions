e787d0cba9ab0dd3ed767f0cea4c93fb931f222c
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-07 20:48:17 -0400

    Time: 99 ms (76.83%) | Memory: 32.2 MB (56.39%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.py
+++ 480-sliding-window-median/sliding-window-median.py
@@ -1,33 +1,65 @@
-from bisect import bisect_left
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         """
-        need to keep sorted list
-        - sortedcontainer then just median
-        - [-1 1 3] bisectleft then insert. window size > k then remove the oldest one
-        - how to remove?  we know OG idx, we know which to remove based on curr idx (idx-k)
-        - remove based on value. its sorted list so exact removal not required
+        2 heaps, similar to one other problem that has to use both min and max heap
         """
-        arr = []
-        q = deque() # store the value to remove
+        maxHeap = [] # stores the left side of the sorted arr
+        minHeap = [] # stores the right side of the sorted arr
+        heapDict = defaultdict(int)
+        # [1 "2" "3" 4] , [1 "-1" -2 2 3] ; max heap always GTE minHeap
+        # [1,3,-1,-3,5,3,6,7] -> [-3 -1] [1 3]
         n = len(nums)
         res = []
-        for i in range(n):
-            if i==0:
-                arr.append(nums[i])
-            else:
-                idx = bisect_left(arr,nums[i])
-                arr.insert(idx,nums[i])
-            q.append(nums[i])
+        for i in range(k):
+            heapq.heappush(maxHeap,-nums[i])
+            heapq.heappush(minHeap, -heapq.heappop(maxHeap))
+            if len(minHeap) > len(maxHeap):
+                heapq.heappush(maxHeap, -heapq.heappop(minHeap))
+        if k%2==1:
+            res.append(-maxHeap[0])
+        else:
+            res.append((-maxHeap[0]+minHeap[0])/2)
+
+        for i in range(k,n):
+            prev = nums[i-k]
+            # need to remove prev
+            heapDict[prev] +=1 # lazy removing. Keep track for now
             
-            if len(arr)>k:
-                oldest = q.popleft()
-                arr.remove(oldest)
-            if len(arr)==k:
-                x = len(arr)
-                if x%2==0:
-                    res.append((arr[x//2]+arr[x//2 - 1])/2)
-                else:
-                    res.append(arr[x//2])
+            # now balance - heaps same values or maxHeap has at most 1 more val than minHeap
+            balance = -1 if prev<=res[-1] else 1
+            # either prev is in maxHeap, meaning we might have equal or 1 less element in maxHeap
+            # or prev is in minHeap, meaning we might have one or two more elements in maxHeap
+            
+            # now process curr num
+            if nums[i]<=res[-1]:
+                balance +=1
+                heapq.heappush(maxHeap,-nums[i])
+            else:
+                balance -=1
+                heapq.heappush(minHeap,nums[i])
+
+            # now balance the heap if balance!=0
+
+            # means max heap has less elements, get from minHeap
+            if balance<0:
+                heapq.heappush(maxHeap, -heapq.heappop(minHeap))
+            elif balance>0:
+                heapq.heappush(minHeap, -heapq.heappop(maxHeap))
+            
+            # now cleanup maxHeap and minHeap - Lazy removal
+            while maxHeap and heapDict[-maxHeap[0]]>0:
+                heapDict[-maxHeap[0]]-=1
+                heapq.heappop(maxHeap)
+
+            while minHeap and heapDict[minHeap[0]]>0:
+                heapDict[minHeap[0]]-=1
+                heapq.heappop(minHeap)
+
+            if k%2==1:
+                res.append(-maxHeap[0])
+            else:
+                res.append((-maxHeap[0]+minHeap[0])/2)
+ 
+            
+        
         return res
-                
