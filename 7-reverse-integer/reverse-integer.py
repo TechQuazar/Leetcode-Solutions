2b526624d86700a4c90748c9fd8b4b14b7436ecc
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-14 09:36:54 -0400

    Time: 44 ms (24.11%) | Memory: 17.9 MB (36.73%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,15 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        low = -2**31
+        # low = -2**31, dont need to check this cause "The minimum 32-bit signed integer is -2147483648, but the maximum reversed positive number is 2147483647, which when negated becomes -2147483647 — still valid."
         high = 2**31 - 1
         curr = 0
         isNeg = True if x<0 else False
         if isNeg:
             x = -1*x
         while x>0:
-            curr= curr*10 + x%10
+            digit = x%10
             x = x//10
-            if not (low<=curr<=high):
+            if curr > high//10 or (curr==high//10 and digit>7):
                 return 0
+            curr = curr*10 + digit
 
         return curr if not isNeg else curr*-1
