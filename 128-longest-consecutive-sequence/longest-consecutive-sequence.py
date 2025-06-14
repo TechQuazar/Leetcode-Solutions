5dadf7d34e236903c5c43c063eac70d6ec0a2244
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-13 20:31:35 -0400

    Time: 179 ms (5.77%) | Memory: 42.7 MB (6.09%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,14 +1,37 @@
-class Solution:
-    def longestConsecutive(self, nums):
-        seen = set(nums)
-        res = 0
-        for num in seen: # if you do for num in nums you get TLE.
-            if num-1 not in seen:
-                curr = num
-                count = 0
-                while curr in seen:
-                    count+=1
-                    curr +=1
-                res = max(res,count)
+class UF:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.rank = [1]*n
 
-        return res
+    def find(self,p):
+        if p!=self.parent[p]:
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+    
+    def union(self,p,q):
+        parP = self.find(p)
+        parQ = self.find(q)
+        if parP!=parQ:
+            if self.rank[parP]>=self.rank[parQ]:
+                self.parent[parQ] = parP
+                self.rank[parP]+=self.rank[parQ]
+            else:
+                self.parent[parP] = parQ
+                self.rank[parQ] += self.rank[parP]
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        nums = list(set(nums))
+        uf = UF(len(nums))
+        numToIdx = defaultdict(int)
+        for i,x in enumerate(nums):
+            numToIdx[x] = i
+            if x-1 in numToIdx:
+                uf.union(i,numToIdx[x-1])
+            if x+1 in numToIdx:
+                uf.union(i,numToIdx[x+1])
+
+        return max(uf.rank)
