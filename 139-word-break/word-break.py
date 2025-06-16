598066683de09de400618dd113cc8516cfb4e061
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-16 14:04:13 -0400

    Time: 3 ms (71.03%) | Memory: 19.4 MB (5.28%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,15 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        """
-        DP botton up
-        """
-        wordSet = set(wordDict)
+        root = TrieNode()
         n = len(s)
-        dp = [False for _ in range(n+1)]
-        dp[0] = True # Empty string is always "breakable"
-        for i in range(1,n+1):
-            for j in range(i):
-                if s[j:i] in wordSet and dp[j]:
-                    dp[i] = True
-                    break
-        return dp[n]
+        for word in wordDict:
+            node = root
+            for ch in word:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+            node.is_end = True
+        
+        @lru_cache(None)
+        def dfs(start):
+            if start==n:
+                return True
+            node = root
+            for i in range(start, n):
+                ch = s[i]
+                if ch not in node.children:
+                    return False
+                node = node.children[ch]
+                if node.is_end and dfs(i+1):
+                    return True
+            return False
+        
+        return dfs(0)
