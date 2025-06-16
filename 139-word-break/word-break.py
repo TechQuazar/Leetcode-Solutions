29b09af16a01f974cecb5adb33e8c966876d7dfd
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-16 13:45:58 -0400

    Time: 7 ms (24.47%) | Memory: 17.9 MB (44.54%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,16 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         """
-        TC O(N**2) SC O(N) call stack
+        DP botton up
         """
         wordSet = set(wordDict)
         n = len(s)
-        @cache
-        def recur(i):
-            if i==n:
-                return True
-            for end in range(i+1,n+1):
-                if s[i:end] in wordSet and recur(end):
-                    return True
-            return False
-        return recur(0)
+        dp = [False for _ in range(n+1)]
+        dp[0] = True # Empty string is always "breakable"
+        for i in range(1,n+1):
+            for j in range(i):
+                if s[j:i] in wordSet and dp[j]:
+                    dp[i] = True
+                    break
+        return dp[n]
