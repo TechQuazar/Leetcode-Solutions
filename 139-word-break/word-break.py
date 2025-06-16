69bc2fe6ff7e6b43b6cd47f224e055ecfeab013b
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-16 13:31:05 -0400

    Time: 6 ms (28.51%) | Memory: 18.3 MB (7.19%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -3,20 +3,19 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordSet = set(wordDict)
         @cache
-        def recur(s,wordTuple):
+        def recur(s):
             if s=="":
                 return True
             n = len(s)
             res = False
             curr = ""
-            wordSet = set(wordTuple)
             for i in range(n):
                 curr+=s[i]
                 if curr in wordSet:
-                    res = res or recur(s[i+1:],wordTuple)
+                    res = res or recur(s[i+1:])
                     if res:
                         return True
             return res
         
-        return recur(s,tuple(wordDict))
+        return recur(s)
         
