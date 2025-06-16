786f68046e1a8ddca890cfdbf5e4b50e9730eed7
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-16 13:37:39 -0400

    Time: 7 ms (24.47%) | Memory: 18 MB (24.26%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,21 +1,16 @@
 class Solution:
-    
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+        TC O(N**2) SC O(N) call stack
+        """
         wordSet = set(wordDict)
+        n = len(s)
         @cache
-        def recur(s):
-            if s=="":
+        def recur(i):
+            if i==n:
                 return True
-            n = len(s)
-            res = False
-            curr = ""
-            for i in range(n):
-                curr+=s[i]
-                if curr in wordSet:
-                    res = res or recur(s[i+1:])
-                    if res:
-                        return True
-            return res
-        
-        return recur(s)
-        
+            for end in range(i+1,n+1):
+                if s[i:end] in wordSet and recur(end):
+                    return True
+            return False
+        return recur(0)
