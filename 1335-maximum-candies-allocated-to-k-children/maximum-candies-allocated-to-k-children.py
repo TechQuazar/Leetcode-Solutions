afecf5ed3c9fc59b3c846293f859b85a4ecdf9ae
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-17 09:56:25 -0400

    Time: 715 ms (5.06%) | Memory: 29.7 MB (69.27%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -1,31 +1,26 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-        '''
-        if sum(candies)<k:
-            return 0
-        else - basically we have atleast 1 candy to give to each child
-        so min = 1
-        max = min(candies)
-        binary search - for number of candies each child can get
-        '''
+        """
+        binary search
+        """
         n = len(candies)
-        if sum(candies)<k:
-            return 0
-        
-        def canGive(target):
-            counter = 0
-            for x in candies:
-                counter+= x//target
-            return False if counter<k else True
-        
-        l,r = 1, max(candies)
+        l = 0
+        r = sum(candies)
         res = 0
+        def canGive(target):
+            if target==0:
+                return True
+            count = 0
+            for cand in candies:
+                total = cand//target
+                count+=total
+            return count>=k
+
         while l<=r:
             mid = (l+r)//2
-            print('l,r,mid',l,r,mid)
             if canGive(mid):
-                res = max(res,mid)
                 l = mid+1
+                res = max(res,mid)
             else:
                 r = mid-1
         return res
