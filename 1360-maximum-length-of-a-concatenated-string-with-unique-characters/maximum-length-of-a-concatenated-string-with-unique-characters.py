f4c903ef9a6e1043f8674ef46872aa5a5f81b05f
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-19 20:16:22 -0400

    Time: 39 ms (82.30%) | Memory: 17.8 MB (62.44%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,47 +1,17 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        # ignore all strings in arr that are not uniq themselves
-        # at each idx, i can take the substring if it doesnt interfere or i can leave
-        # recursion will return the length of longest uniq that can be formed
-        newList = []
-        for s in arr:
-            temp = [0]*26
-            ignore = False
-            for ch in s:
-                x = ord(ch)-ord('a')
-                temp[x]+=1
-                if temp[x]>=2:
-                    ignore = True
-                    break
-            if not ignore:
-                newList.append(temp)
-        arr = newList
-        n = len(arr)
-        cache = {}
-        def recur(i, currStr):
-            if i==n:
-                return 0
-            # take if no interference with prev
-            if (i,currStr) in cache:
-                return cache[(i,currStr)]
-            res = 0
-            take,leave = 0,0
-            ignore = False
-            for j,x in enumerate(arr[i]):
-                if currStr[j]>0 and x>0:
-                    ignore = True
-                    break
-            if not ignore:
-                count = 0
-                temp = list(currStr)
-                for j,x in enumerate(arr[i]):
-                    if x>0:
-                        count+=1
-                        temp[j]+=1
-                take = count + recur(i+1,tuple(temp))
-            leave = recur(i+1,tuple(currStr))
-            cache[(i,currStr)] = max(take,leave)
-            return cache[(i,currStr)]
-        
-        return recur(0,tuple([0]*26))
+        def isUnique(s):
+            return len(set(s))==len(s)
 
+        def backtrack(idx, path):
+            nonlocal maxLen
+            if len(path)>maxLen:
+                maxLen = len(path)
+            for i in range(idx,len(arr)):
+                combined = path + arr[i]
+                if isUnique(combined):
+                    backtrack(i+1,combined)
+        
+        maxLen = 0
+        backtrack(0,"")
+        return maxLen
