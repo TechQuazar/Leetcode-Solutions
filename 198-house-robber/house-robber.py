9c8a1d1900d0b29e2febeff0d0674b5e0f458c41
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-06-23 16:26:43 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.69%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,17 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        cache = {}
-        def recur(i):
-            if i>=n:
-                return 0
-            if i in cache:
-                return cache[i]
-            take,leave = 0,0
-            take = nums[i]+recur(i+2)
-            leave = recur(i+1)
-            cache[i]= max(take,leave)
-            return cache[i]
-    
-        return recur(0)
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0],nums[1])
         
+        dp = [0]*(n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i] = max(nums[i]+dp[i-2],dp[i-1])
+        return dp[-1]
