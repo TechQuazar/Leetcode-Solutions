6bfd026a80f13a9a31d450af938d2a8743468804
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-07-01 09:00:20 -0400

    Time: 0 ms (100.00%) | Memory: 19.9 MB (19.66%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,26 +1,30 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # insert and merge
+        # find where it can be inserted, insert, then for others coming in merge
         n = len(intervals)
-        si, ei = newInterval
+        start,end = newInterval
         res = []
-        overlapIdx = float('inf')
         inserted = False
-
-        for i in range(n):
-            start, end = intervals[i]
-            if ei < start:
-                res.append([si, ei])
-                return res+intervals[i:]
-            if si > end:
-                res.append([start, end])
+        for i,x in enumerate(intervals):
+            si,ei = x
+            if ei<start or si>end:
+                res.append([si,ei])
             else:
-                # there is overlap
-                overlapIdx = min(overlapIdx, i)
-                si = min(si, start)
-                ei = max(ei, end)
+                if not inserted:
+                    end = max(end,ei)
+                    start = min(si,start)
+                    res.append([start, end])
+                    inserted = True
+                else:
+                    res[-1][1] = max(end,ei)
 
-        if overlapIdx == float('inf'):
-            res.append([si, ei])
-        elif overlapIdx != float('inf'):
-            res.insert(overlapIdx, [si, ei])
+            if not inserted and start>ei and i+1<n and end<intervals[i+1][0]:
+                res.append(newInterval)
+                inserted = True
+        if not inserted:
+            if res and end<res[0][0]:
+                res.insert(0,newInterval)
+            else:
+                res.append(newInterval)
         return res
