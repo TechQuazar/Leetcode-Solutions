f8418f3cf91893ba3d442991fe290965d5bffb13
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-07-01 18:58:49 -0400

    Time: 1034 ms (24.50%) | Memory: 23.3 MB (21.54%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -6,21 +6,21 @@ class Solution:
             return -1
         
         n = len(coins)
+        dp = [-1 for _ in range(amount+1)]
 
-        dp = [[-1 for _ in range(amount)] for _ in range(n)]
-        def recur(i,total):
+        def recur(total):
+            if total>amount:
+                return float('inf')
             if total==amount:
                 return 0
-            if total>amount or i>=n:
-                return float('inf')
-            if dp[i][total]!=-1:
-                return dp[i][total]
+            if dp[total]!=-1:
+                return dp[total]
             res = float('inf')
-            if coins[i]+total<=amount:
-                res = min(res, 1+recur(i,total+coins[i]))
-            res = min(res, recur(i+1, total))
-            dp[i][total] = res
+            for coin in coins:
+                if total+coin<=amount:
+                    res = min(res, 1+recur(total+coin))
+            dp[total] = res
             return res
-
-        res =  recur(0,0)
+        
+        res =  recur(0)
         return res if res!=float('inf') else -1
