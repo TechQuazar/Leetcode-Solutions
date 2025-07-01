36fe3c8b8655a821ec9b72f5acf55fae5d0ee017
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-07-01 19:44:25 -0400

    Time: 7 ms (83.31%) | Memory: 17.9 MB (61.65%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,30 +1,21 @@
 class Solution:
     def combinationSum(self, arr: List[int], target: int) -> List[List[int]]:
-        '''
-        1. at idx, is curr + val[idx]<=target? go for it
-        2. Additionally, just skip it cause you can
-        3. base when target==0 add to set
-        4. maintain set to ensure uniq answers
-        '''
         n = len(arr)
-        res = set()
-        def recur(i,curr, path):
-            nonlocal res
-            if curr==target:
-                res.add(path)
+        res = []
+        def backtrack(i,total, path):
+            if i>=n or total>target:
                 return
-            if i>=n or curr>target:
+            if total==target:
+                res.append(list(path))
                 return
-            if curr+arr[i]<=target:
-                # print('i,curr,path',i,curr,path)
-                newPath = list(path)
-                newPath.append(arr[i])
-                recur(i,curr+arr[i],tuple(newPath))
-            # print('out:i,curr,path',i,curr,path)
-            recur(i+1,curr,path)
-        
-        recur(0,0,())
-        ans = []
-        for each in res:
-            ans.append(list(each))
-        return ans
+            if arr[i]+total<=target:
+                path.append(arr[i])
+                backtrack(i, total+arr[i],path)
+                path.remove(arr[i])
+            
+            backtrack(i+1, total, path)
+            return
+
+
+        backtrack(0,0,[])
+        return res
