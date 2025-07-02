e20a0de2643148a13c38a71f7426959d0d8738f4
PranavMohril <39716095+TechQuazar@users.noreply.github.com>
2025-07-02 13:11:03 -0400

    Time: 2090 ms (17.57%) | Memory: 33.9 MB (33.62%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -10,14 +10,14 @@ class Solution:
 
         # if sum ==0, then i==0 is True
         dp[0][0] = True
-        for i in range(n+1):
+        for i in range(1,n+1):
             for currSum in range(partSum+1):
                 # can we for the currSum without including the current number -> nums[i-1]?
                 if dp[i-1][currSum]:
                     dp[i][currSum] = True
                 if nums[i-1]<=currSum and dp[i-1][currSum-nums[i-1]]:
                     dp[i][currSum] = True
-                    
+
         return dp[-1][-1]
 
         
